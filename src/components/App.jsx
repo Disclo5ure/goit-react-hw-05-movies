e7422444c52eb4b1c4cb11c53ef6ce3822be4729
Disclo5ure <46845468m@gmail.com>
2023-09-23 12:52:45 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,12 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="*" element={<Home />} />
         <Route path="movies/:id" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="movies" element={<Movies />} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
